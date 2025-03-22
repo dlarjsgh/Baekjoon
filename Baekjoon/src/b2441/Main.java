a86ffce90b5eacc0b741ba610f0c356460897006
Geonho Lim <dlarjsgh67@naver.com>
2025-03-22 23:56:31 +0900

    20250322

--- Baekjoon/src/b2441/Main.java
+++ Baekjoon/src/b2441/Main.java
@@ -1,30 +1,27 @@
-package b1427;
+package b2441;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.TreeSet;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		int a = Integer.parseInt(br.readLine());
 		
-		String s = br.readLine();
-		int slength = s.length();
-		int[] array = new int[slength];
-		
-		for(int i=0; i<slength; i++) {
-			array[i] = s.charAt(i);
-		}
-		
-		Arrays.sort(array);
-		
-		for(int i=slength-1; i>=0; i--) {
-			sb.append(array[i]-48);
+		for(int i=0; i<a; i++) {
+			for(int p=a; p>a-i; p--) {
+				sb.append(" ");
+			}
+			for(int j=0; j<a-i; j++) {	
+				sb.append("*");
+			}
+			sb.append("\n");
 		}
 		
 		System.out.print(sb);
+
+		
 	}
 }
